dc7824a277fc198fbb450d88c07331a3546f0eba
ElizabethAce <acev.elizabeth3@gmail.com>
2025-10-22 22:56:20 -0700

    Small edits

--- src/BLE_server.c
+++ src/BLE_server.c
@@ -8,7 +8,7 @@
  * 
  * Purpose: This program is the server side (Peripheral) of the bluetooth communication 
  *          between my device and the app. The server advertises itself for 
- *          clients (Central) to connect. It exposes a GATT Service containing data 
+ *          clients (Central) to connect. It exposes a GATT (Generic Attribute Profile) Service containing data 
  *          like sensor readings.
  * 
  * 
@@ -37,12 +37,12 @@ static uint16_t service_handle;
 static esp_gatt_srvc_id_t service_id;
 static esp_gatt_if_t gatts_if_global;
 static esp_ble_adv_params_t adv_params = {
-    .adv_int_min = 0x20,
+    .adv_int_min = 0x20,                                    // How often to advertise (~20-40 ms)
     .adv_int_max = 0x40,
-    .adv_type = ADV_TYPE_IND,
-    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,
-    .channel_map = ADV_CHNL_ALL,
-    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY,
+    .adv_type = ADV_TYPE_IND,                               // Connectable, general advertising
+    .own_addr_type = BLE_ADDR_TYPE_PUBLIC,                  // Use device's public address
+    .channel_map = ADV_CHNL_ALL,                            // Advertise on all 3 BLE channels
+    .adv_filter_policy = ADV_FILTER_ALLOW_SCAN_ANY_CON_ANY, // Allow anyone to scan or connect
 };
 
 /* FUNCTION PROTOTYPES */
@@ -50,12 +50,10 @@ void gatts_event_handler(esp_gatts_cb_event event, esp_garr_if_t gatts_if, esp_b
 void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param);
 
 void app_main() {
-    /* Turn on BLE */
+    /* Configure Bluetooth */
     // Initialize NVS 
     nvs_flash_init();   // Stores pairing and bonding info
-    
-
-    esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT); // Releases BSS, data for BLE use only after de-initializing
+    esp_bt_controller_mem_release(ESP_BT_MODE_CLASSIC_BT); // Release Classic for BLE use only to save RAM
     // Get the default configuration
     esp_bt_controller_config_t bt_cfg = {
         .controller_task_prio = 5,
@@ -65,17 +63,14 @@ void app_main() {
     // Initialize controller
     esp_bt_controller_init(&bt_cfg);
     esp_bt_controller_enable(ESP_BT_MODE_BLE);
-    
-    
+    // Initialize bluedroid (software stack)
     esp_bluedroid_init();
     esp_bluedroid_enable();
 
     /* Create a BLE Device */
     esp_ble_gatts_register_callback(gatts_event_handler);
     esp_ble_gap_register_callback(gap_event_handler);
-    esp_ble_gatts_app_register(0);
-
-    /* Create a BLE Server */
+    esp_ble_gatts_app_register(0);      // Registers a BLE GATT server application with an app ID of 0
 
     /* Create a BLE Service */
     esp_ble_gatts_create_service(gatts_if, &service_id, GATTS_NUM_HANDLE);
@@ -84,15 +79,14 @@ void app_main() {
     esp_ble_gatts_add_char(service_handle, &char_uuid, perm, prop, NULL, NULL);
 
     /* Create a BLE Descriptor */
-    esp_ble_gatts_add_char_descr(service_handle, &descr_uuid, perm, NULL, NULL);
+    esp_ble_gatts_add_char_descr(service_handle, &descr_uuid, perm, NULL, NULL); // Enables metadata like notifications
 
-    /* Start Service */
+    /* Start Service and Advertising */
     esp_ble_gatts_start_service(service_handle);
-
-    /* Start Advertising */
     esp_ble_gap_start_advertising(&adv_params);
 }
 
+/* Handles GATT server events (e.g. client connection, client requests to r/w data, service created) */
 void gatts_event_handler(esp_gatts_cb_event event, esp_garr_if_t gatts_if, esp_ble_gatts_cb_param_t *param) {
     switch (event) {
         case ESP_GATTS_REG_EVT:
@@ -110,11 +104,13 @@ void gatts_event_handler(esp_gatts_cb_event event, esp_garr_if_t gatts_if, esp_b
             ESP_LOGI("BLE", "Device Disconnected");
             esp_ble_gap_start_advertising(&adv_params);
             break;
+        
         default:
             break;
     }
 }
 
+/* Handles advertising and connection events (e.g. Displaying updates, scanning for nearby devices, connection updates, authentication requests, and advertising.) */
 void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) {
     if (event == ESP_GAP_BLE_ADV_START_COMPLETE_EVT) {
         ESP_LOGI("BLE", "Advertising started");
